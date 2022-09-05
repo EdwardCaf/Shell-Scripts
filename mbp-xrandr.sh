3b149fd79839c20c7dc16ff77ba3b257990222a0
Edward <ed_caf@protonmail.com>
2022-09-05 07:42:41 -0400

    Add xrandr resolution

---
+++ mbp-xrandr.sh
@@ -0,0 +1,5 @@
+gtf 3024 1964 60
+xrandr --newmode "3024x1964_60.00"  509.14  3024 3264 3600 4176  1964 1965 1968 2032  -HSync +Vsync
+xrandr --addmode Virtual-1 "3024x1964_60.00"
+xrandr --output Virtual-1 --mode "3024x1964_60.00" 
+xrandr --dpi 254
